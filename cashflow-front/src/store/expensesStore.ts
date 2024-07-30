0ca638f0377e10365bfe638c559678a8a0fc039b
guilhermedelrio <guilherme@gplpm.com.br>
2024-07-30 19:48:01 -0300

    Adicionando composable generico para requisicoes http

--- cashflow-front/src/store/expensesStore.ts
+++ cashflow-front/src/store/expensesStore.ts
@@ -1,18 +1,21 @@
 import { defineStore } from "pinia";
-import api from "../api/axios";
 import Expense from "../models/Expense";
+import { useFetch } from "../composables/crudComposable";
 
 export const useExpensesStore = defineStore('expenses', {
   state: () => ({
     expenses: [] as Expense[],
   }),
   actions: {
-    async getAllExpenses() : Promise<Expense[]>{
-      try {
-        const res = await api.get('/Expense/GetAllExpenses');
-        this.expenses = res.data;
-      } catch(e) {
-        console.log(e);
+    async getAllExpenses(): Promise<Expense[]> {
+      const { data, error, fetchData } = useFetch<Expense[]>('/Expense/GetAllExpenses');
+
+      await fetchData();
+
+      if (error.value) {
+        console.log(error.value);
+      } else {
+        this.expenses = data.value || [];
       }
 
       return this.expenses;
